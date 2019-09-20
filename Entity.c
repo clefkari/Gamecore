738af9157efdafc2ec97da81f1b9dd6861a8897a
Christopher Lefkarites <Whitesnake@Christophers-MacBook-Pro-2.local>
2019-09-20 06:36:08 -0700

    Console Changes

--- Entity.c
+++ Entity.c
@@ -7,23 +7,23 @@
 static unsigned int entityCounter = 0;
 
 /* We create a new entity by using a stack entity which will recieve the input
-from user or file. */
+   from user or file. */
 
 void * copyEntity(void * data){
 
-	Entity * ep = (Entity*)malloc(sizeof(Entity));
+  Entity * ep = (Entity*)malloc(sizeof(Entity));
 
-	ep->texture = ((Entity*)data)->texture;
-	ep->x = ((Entity*)data)->x;
-	ep->y = ((Entity*)data)->y;
-	ep->w = ((Entity*)data)->w;
-	ep->h = ((Entity*)data)->h;
+  ep->texture = ((Entity*)data)->texture;
+  ep->x = ((Entity*)data)->x;
+  ep->y = ((Entity*)data)->y;
+  ep->w = ((Entity*)data)->w;
+  ep->h = ((Entity*)data)->h;
 
-	ep->id = entityCounter++;
-	
-	fprintf(stderr,NEW_ENTITY,(void*)ep,((Entity*)data)->id);
+  ep->id = entityCounter++;
 
-	return (void*) ep;
+  fprintf(stderr,NEW_ENTITY,(void*)ep,((Entity*)data)->id);
+
+  return (void*) ep;
 
 }
 
@@ -33,10 +33,10 @@ void * copyEntity(void * data){
 
 void deleteEntity(void ** data){
 
-	fprintf(stderr,DELETE_ENTITY,*data,((Entity*)*data)->id);
-	
-	free(*data);
-	*data=0;
+  fprintf(stderr,DELETE_ENTITY,*data,((Entity*)*data)->id);
+
+  free(*data);
+  *data=0;
 
 }
 
@@ -46,10 +46,10 @@ void deleteEntity(void ** data){
 
 int equalsEntity(void * lhs, void * rhs){
 
-	Entity * elhs = (Entity *)lhs;
-	Entity * erhs = (Entity *)rhs;
+  Entity * elhs = (Entity *)lhs;
+  Entity * erhs = (Entity *)rhs;
 
-	return (elhs->id == erhs->id);
+  return (elhs->id == erhs->id);
 
 }
 
@@ -66,16 +66,16 @@ int initEntity(){
 }
 
 int parseEntity(char * cmd){
-  
+
   /* Stack Struct to store input. */
   Entity entity = { 0 };
-  
+
   /* For processing the command. */
   char * ch, * endptr;
 
   errno = 0;
   ch = strtok(cmd,DELIM);
-  
+
   /* Cancel when nothing is entered. */
   if(!ch){
     return -1;
@@ -91,7 +91,7 @@ int parseEntity(char * cmd){
     fprintf(stderr,ENTITY_FAIL,strerror(errno));
     return -1;  
   }
-  
+
   ch = strtok(NULL,DELIM);
   if(errno){
     fprintf(stderr,ENTITY_FAIL,strerror(errno));
@@ -102,7 +102,7 @@ int parseEntity(char * cmd){
     fprintf(stderr,ENTITY_FAIL,strerror(errno));
     return -1;  
   }
-  
+
   ch = strtok(NULL,DELIM);
   if(errno){
     fprintf(stderr,ENTITY_FAIL,strerror(errno));
@@ -113,7 +113,7 @@ int parseEntity(char * cmd){
     fprintf(stderr,ENTITY_FAIL,strerror(errno));
     return -1;  
   }
-  
+
   ch = strtok(NULL,DELIM);
   if(errno){
     fprintf(stderr,ENTITY_FAIL,strerror(errno));
@@ -124,7 +124,7 @@ int parseEntity(char * cmd){
     fprintf(stderr,ENTITY_FAIL,strerror(errno));
     return -1;  
   }
-  
+
   ch = strtok(NULL,DELIM);
   if(errno){
     fprintf(stderr,ENTITY_FAIL,strerror(errno));
@@ -138,4 +138,5 @@ int parseEntity(char * cmd){
   insert_List(entityList,&entity);
 
   return 0;
+
 }
--- Gamecore.c
+++ Gamecore.c
@@ -3,109 +3,81 @@
 #############################################################################*/
 
 #include "Gamecore.h"
-	
+
 int run(){
-	SDL_Window *window;
-	SDL_Renderer *renderer;
-	SDL_Texture *texture;
-	SDL_Event event;
-	SDL_Rect d;
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  SDL_Texture *texture;
+  SDL_Event event;
+  SDL_Rect d;
 
   Node * np;
-  
+
   char buffer [BUFFER_SIZE] = { 0 };
-	char * ch;
+  char * ch;
 
   int running = 1;
 
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
-		return 3;
-	}
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+    return 3;
+  }
 
-	window = SDL_CreateWindow("Gamecore",
-			SDL_WINDOWPOS_UNDEFINED,
-			SDL_WINDOWPOS_UNDEFINED,
-			SCREEN_WIDTH, SCREEN_HEIGHT,
-			SDL_WINDOW_RESIZABLE);
+  window = SDL_CreateWindow("Gamecore",
+      SDL_WINDOWPOS_UNDEFINED,
+      SDL_WINDOWPOS_UNDEFINED,
+      SCREEN_WIDTH, SCREEN_HEIGHT,
+      SDL_WINDOW_RESIZABLE);
 
 
-	renderer = SDL_CreateRenderer(window, -1, 0);
+  renderer = SDL_CreateRenderer(window, -1, 0);
 
   /* Global Data List Initialization. */
-	initTextures(renderer);
+  initTextures(renderer);
   initEntity();
 
-	/* texture = getTexture("water.bmp"); */
+  /* texture = getTexture("water.bmp"); */
 
-	/* End Me */
+  /* End Me */
 
-	while (running) {
+  while (running) {
 
-		/* Random Texture Test -
-			 switch(rand()%5){
-			 case 0:
-			 texture = getTexture("tree.bmp");
-			 break;
+    while(SDL_PollEvent(&event)){
 
-			 case 1:
-			 texture = getTexture("rock.bmp");
-			 break;
+      /* Window Closing Event */
 
-			 case 2:
-			 texture = getTexture("water.bmp");
-			 break;
+      if(event.type == SDL_QUIT){
 
-			 case 3:
-			 texture = getTexture("voodoo.bmp");
-			 break;
+        running = 0;
 
-			 case 4:
-			 texture = getTexture("flameblast.bmp");
-			 break;
+      }
 
-			 default:
-			 break;
-			 }
+      /* Key Down Events */
 
+      if(event.type == SDL_KEYDOWN){
+        
+        /* Parse a Console Command */
 
-			 d.x= rand()%500;
-			 d.y= rand()%500;
-			 d.w= rand()%500;
-			 d.h= rand()%500;
+        if(event.key.keysym.sym == SDLK_BACKQUOTE){
 
-		 */
-
-		while(SDL_PollEvent(&event)){
-
-			if(event.type == SDL_QUIT){
-
-				running = 0;
-
-			}
-			
-			/* Key Down Events */
-
-			if(event.type == SDL_KEYDOWN){
-
-				if(event.key.keysym.sym == SDLK_BACKQUOTE){
-
-					fgets(buffer,BUFFER_SIZE,stdin);//TODO - insert console commands here.
+          fgets(buffer,BUFFER_SIZE,stdin);
           ch = strchr(buffer,NEWLINE);
-          
+
           if(ch){
+
             *ch = 0;
-				    parseEntity(buffer);
+            parseEntity(buffer);
+          
           }
 
-				}
+        }
 
-			}
+      }
 
-		}
+    }
 
-		SDL_RenderClear(renderer);
-		
+    SDL_RenderClear(renderer);
+
     /* Boiler Plate */
 
     np = entityList->front;
@@ -120,7 +92,7 @@ int run(){
 
         SDL_RenderCopy(renderer, ((Entity*)(np->data))->texture, 
             NULL, &d);
-        
+
         np = np->pre;
 
       }
@@ -131,17 +103,17 @@ int run(){
 
     SDL_RenderPresent(renderer);
 
-	}
+  }
 
 
   /* Global Data List Deallocation. */
-	delete_List(&textureList);
+  delete_List(&textureList);
   delete_List(&entityList);
 
   /* SDL Clean up. */
-	SDL_DestroyRenderer(renderer);
-	SDL_Quit();
+  SDL_DestroyRenderer(renderer);
+  SDL_Quit();
 
-	return 0;
+  return 0;
 
 }
